54cf5e53dcc1019c59c07ba8f2621f80b4f20f49
tangbin <tang123>
2015-09-05 21:59:36 +0800

    优化

--- app/src/main/java/com/tb/demo/utils/hangview/MainActivity.java
+++ app/src/main/java/com/tb/demo/utils/hangview/MainActivity.java
@@ -5,7 +5,6 @@ import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
@@ -25,7 +24,7 @@ public class MainActivity extends Activity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        top_img=(ImageView)findViewById(R.id.top_img);
+        top_img = (ImageView) findViewById(R.id.top_img);
         bar = (ViewGroup) findViewById(R.id.bar);
         bar_fixed = (ViewGroup) findViewById(R.id.bar_fixed);
         bar_float = (ViewGroup) findViewById(R.id.bar_float);
